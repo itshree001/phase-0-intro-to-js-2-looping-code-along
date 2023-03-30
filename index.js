222e9c8bb929855071a6ff5d521dd74ad911bb8a
Krishna <itshree001@gmail.com>
2023-03-30 13:16:29 +1100

    returns an array of thank you messages for each name provided to the function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(name){
+    console.log(`Thank you, ${name}, for the wonderful surprise gift!`)
+}
+writeCards(names[0]);
+writeCards(names[1]);
+writeCards(names[2]);
+
+
+
+function countDown(num){
+    for(let i=0; num >= i; num--){
+        console.log(num)
+    }
+
+}
+countDown(10)
+countDown(4)
+
